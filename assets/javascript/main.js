091ad5fce537961c8c2b6f6114466cfa6f315f48
Lee Lawler <lawler_lee@icloud.com>
2020-01-29 21:39:19 -0600

    can now pause and play gifs on click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,22 @@
 var topics = ["husky", "pug", "chihuahua", "rottweiler"];
 
-// event listener for buttons
+generateTopics();
 
-$("button").on("click", function () {
-    var doggo = $(this).attr("data-dog");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + doggo + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+// event listener for buttons
+function generateTopics() {
+    var buttonsArray = topics.map(t => "<button class='topicButton'>" + t + "</button>");
+    console.log(buttonsArray)
+    $("#buttonsWrapper").append(buttonsArray)
+};
+
+$(".topicButton").on("click", function () {
+    console.log($(this))
+    var topic = $(this).text();
+    search(topic);
+});
+
+function search(topic) {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -13,16 +25,16 @@ $("button").on("click", function () {
         console.log(response)
         var results = response.data; //data is an array being stored in the variable results
 
-// looping through results
+        // looping through results
 
         for (i = 0; i < results.length; i++) {
             if (results[i] !== "r" && results[i] !== "pg-13") {
                 var gifBox = $("<div>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var doggoImage = $("<img>");
+                var doggoImage = $("<img class='gif'>");
 
-                doggoImage.attr("src", results[i].images.fixed_height.url);
+                doggoImage.attr("src", results[i].images.fixed_height_still.url);
 
                 gifBox.append(p);
                 gifBox.append(doggoImage);
@@ -30,5 +42,25 @@ $("button").on("click", function () {
                 $("#gifDump").prepend(gifBox);
             };
         };
+
+        // event listener for gifs
+
+        $(".gif").on("click", function () {
+
+            var src = $(this).attr("src").replace(".gif", "");
+
+            // if (src.includes("_s")) {
+            //     src = src.replace("_s", "")
+            // } else {
+            //    src = src.concat("_s")
+            // };
+
+            src = src.includes("_s") ? src.replace("_s", "") : src.concat("_s")
+
+            src = src.concat(".gif")
+
+            $(this).attr("src", src);
+
+        });
     });
-});
+};
